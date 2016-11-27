7945588d796b250b445b508ec59dfbd784c59353
Pushpak Athawale <athawale.pushpak@gmail.com>
2016-11-27 19:58:15 +0530

    first commit

---
+++ CookpadWeatherApp/WeatherTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherTableViewCell.swift
+//  CookpadWeatherApp
+//
+//  Created by Pushpak Athawale on 27/11/16.
+//  Copyright © 2016 com.pushpak. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
